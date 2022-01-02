b11fd27384e29edde6ced06af29ad19e7eabb5be
mknaw <mknawara@gmail.com>
2022-01-02 10:26:53 -0500

    day19

--- Utils.hs
+++ Utils.hs
@@ -1,6 +1,9 @@
 module Utils
-    ( splitChar,
+    ( splitChar
+    , splitString
     ) where
+
+import           Data.List (findIndex, isPrefixOf, tails)
 
 splitChar :: Char -> String -> [String]
 splitChar c s =
@@ -10,3 +13,15 @@ splitChar c s =
       where
         (w, s'') = break (== c) s'
 
+-- Pilfered from stackoverflow
+findString :: (Eq a) => [a] -> [a] -> Maybe Int
+findString search str = findIndex (isPrefixOf search) (tails str)
+
+splitString :: String -> String -> [String]
+splitString sp s
+    | sp `isPrefixOf` s = splitString sp . drop (length sp) $ s
+    | s == "" = []
+    | otherwise =
+        case findString sp s of
+          Just idx -> take idx s : splitString sp (drop (idx + length sp) s)
+          Nothing  -> [s]
